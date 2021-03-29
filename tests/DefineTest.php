6a9cf398163ca65423cb9071bb7e73931d8a9b9b
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-03-30 00:03:42 +0200

    first commit

---
+++ tests/DefineTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Javanile\Define\DefaultCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+test('example', function () {
+    $app = new Application();
+    $app->add(new DefaultCommand());
+
+    $tester = new CommandTester($app->find('define'));
+
+    $statusCode = $tester->execute(['name' => 'tests/fixtures/example1.def']);
+
+    expect(true)->toBeTrue();
+});
